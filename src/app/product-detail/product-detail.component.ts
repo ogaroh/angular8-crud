5db7275c14aedad4a4e6bcdcd41ace03662d8c3f
ogaroh <ogaroerick1998@gmail.com>
2019-08-06 16:45:23 +0300

    crud

---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Product } from '../model/product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  product: Product = { _id: '', prod_name: '', prod_desc: '', prod_price: null, updated_at: null };
+  isLoadingResults = true;
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
+
+  ngOnInit() {
+    this.getProductDetails(this.route.snapshot.params['id']);
+  }
+  // get product details
+  getProductDetails(id: any) {
+    this.api.getProduct(id)
+      .subscribe((data: any) => {
+        this.product = data;
+        console.log(this.product);
+        this.isLoadingResults = false;
+      });
+  }
+}
